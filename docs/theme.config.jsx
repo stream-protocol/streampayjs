76f461f75818f23e4844fb21fe586a639372fbf2
StreamPay <105049869+stream-protocol@users.noreply.github.com>
2023-09-28 15:21:33 +0300

    FIxed packages and doc

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-  logo: <span>SolanaJS</span>,
+  logo: <Image src="/public/streampayjs.svg" alt="" />,
   project: {
     link: 'https://github.com/shuding/nextra',
   },
-  // ...
+  // ... other configurations
 };
